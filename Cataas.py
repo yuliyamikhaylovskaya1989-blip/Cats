278e4c3bd22f0112ec781f79bac24b37fb2e56b4
Юлия <yuliyamikhaylovskaya1989@gmail.com>
2025-08-26 16:18:57 +0300

    функция получения картинки

--- Cataas.py
+++ Cataas.py
@@ -3,6 +3,20 @@ from PIL import  Image, ImageTk
 import requests
 from io import BytesIO
 
+
+def loade_image():
+    try:
+        response=reqests.get(url)
+        response.raise_for_status()
+        image_data=BytesIO(response.content)
+        img=Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+        return  None
+
+
+
 window=Tk()
 window.title("Cats!")
 window.geometry("600x480")
